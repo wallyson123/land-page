4d1d9bab9703b7bd36010cbbc6f614dbbe265ee4
Wálisson Soares <wallyson2013@hotmail.com>
2022-11-23 22:45:30 -0300

    Add files via upload

---
+++ js/obrigado.js
@@ -0,0 +1,7 @@
+// ==================== typing animation ========
+var typed = new Typed(".typing" , {
+    strings:["" ,"Te " , "Te ver em breve" , " Obrigado !"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
